f91b214d72f7319b90d6ff50901d77158d56e1ed
saulm96 <saulmorahernandez96@gmail.com>
2024-10-24 16:13:39 +0200

    fixed filtering script

--- scripts/apiCalls.js
+++ scripts/apiCalls.js
@@ -1,16 +1,36 @@
 const BASE_URL = 'https://api.magicthegathering.io/v1/';
 
-async function fetchCards(cardName) {
+async function fetchData(route, searchParams = {}) {
     try {
-        const response = await fetch(`${BASE_URL}cards?name=${cardName}`);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+        const url = new URL(BASE_URL + route);
+        for (const key of Object.keys(searchParams)) {
+            url.searchParams.append(key, searchParams[key]);
         }
-        const data = await response.json();
-        console.log(data.cards); // Muestra las cartas en consola
+        const response = await fetch(url);
+        const responseData = await response.json();
+        return responseData
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error(error);
+        return { error: error }
     }
 }
 
-fetchCards();
+async function getAllTypes() {
+    const allTypes = await fetchData('types');
+    console.log(allTypes);
+    return allTypes;
+}
+
+async function cardFilter(params) {
+    // Aquí ya no necesitas toString() porque fetchData maneja los parámetros
+    const filteredCards = await fetchData('cards', params);
+    console.log(filteredCards);
+    return filteredCards;
+}
+const params = {
+    rarity: 'Uncommon',
+    power:'2',
+    toughness:'2',
+    set: 'SIR'
+};
+cardFilter(params);
